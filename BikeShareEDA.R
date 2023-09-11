1c87f6c632ab22c14289a6b8897450c59d06c173
Brooke Holtom <Brooke Holtom>
2023-09-11 14:02:38 -0600

    Added R File

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -1,4 +1,20 @@
 library(tidyverse)
 library(vroom)
+library(patchwork)
 
-bike <- vroom()
+bike <- vroom("train.csv")
+
+dplyr::glimpse(bike)
+#plot1 <- DataExplorer::plot_intro(bike)
+#plot3 <- DataExplorer::plot_correlation(bike) 
+#plot4 <- DataExplorer::plot_bar(bike) 
+DataExplorer::plot_histrograms(bike) 
+DataExplorer::plot_missing(bike)
+GGally::ggpairs(bike)
+
+
+plot1 <- ggplot(data=bike, aes(x=temp, y=count)) + geom_point() + geom_smooth(se=FALSE)
+plot2 <- ggplot(data=bike, aes(x=humidity, y=windspeed)) + geom_point() + geom_smooth(se=FALSE)
+plot3 <- ggplot(data=bike, aes(x=casual, y=registered)) + geom_point() + geom_smooth(se=FALSE)
+plot4 <- ggplot(data=bike, aes(x=season, y=count)) + geom_point() 
+(plot1 + plot2) / (plot3 + plot4) #4 panel plot
