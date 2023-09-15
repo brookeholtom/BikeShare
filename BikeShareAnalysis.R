34d48773491d70332f2b5a591aea93bad041d000
Brooke Holtom <Brooke Holtom>
2023-09-15 15:07:00 -0600

    added/updated all docs

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -3,27 +3,49 @@ library(tidymodels)
 library(vroom)
 
 bike <- vroom("train.csv") #importing the data via vroom
+bike_test <-  vroom("test.csv") #importing test data
 
 
 #cleaning section: changing the single "4" weather day to "3"
 bike <- bike %>% 
-         mutate(weather = ifelse(weather >= 4, 3, weather)) 
+         mutate(weather = ifelse(weather >= 4, 3, weather)) %>%
+         select(-casual, -registered)
+bike_test <- bike_test %>%
+  mutate(weather = ifelse(weather >= 4, 3, weather))
 
 
 #feature engineering section via recipe:  
 my_recipe <- recipe(count ~ ., data=bike) %>% 
-  step_date(datetime, features="dow") %>% #adding day of week column 
-  step_date(datetime, features="month") %>% #adding month column
-  step_num2factor(season, transform = function(x) x, levels=c("spring", "summer", "fall", "winter")) %>% #modifying season column from numbers to a factor
-  step_num2factor(weather, transform = function(x) x, levels=c("clear", "mist", "rain/snow")) %>% #modifying weather from numbers into factors
+  step_num2factor(season, levels=c("spring", "summer", "fall", "winter")) %>% #modifying season column from numbers to a factor
+  step_num2factor(weather, levels=c("clear", "mist", "rain/snow")) %>% #modifying weather from numbers into factors
   step_bin2factor(holiday) %>% #modifying holiday to factor from numbers
-  step_bin2factor(workingday) %>% #modifying working day to factor from numbers 
-  step_rm(c(casual, registered)) #removing the casual and registered columns, not needed 
+  step_bin2factor(workingday) %>%
+  step_rm(datetime)#modifying working day to factor from numbers 
 
 #updating table to reflect above changes, will be saved with original name "bike"
 prepped_recipe <- prep(my_recipe) 
-bike_clean <- bake(prepped_recipe, new_data=bike)
+bake(prepped_recipe, new_data=bike)
 
 
+#creating a model
+my_mod <- linear_reg() %>% 
+  set_engine("lm") 
+
+bike_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data = bike)
+
+bike_predictions <- predict(bike_workflow,
+                            new_data=bike_test) 
 
 
+bike_predictions[bike_predictions<0] <- 0 #making all negative columns 0
+
+bike_predictions <- rename(bike_predictions, count = .pred)
+
+
+bike_predictions$datetime <- bike_test$datetime
+bike_predictions$datetime <-format(bike_predictions$datetime)
+
+vroom_write(bike_predictions, "bike_predictions.csv", delim=",")
