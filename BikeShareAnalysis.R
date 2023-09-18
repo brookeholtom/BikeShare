906d07bea026bb9dfa4c571fd2e741ce55146d1c
Brooke Holtom <Brooke Holtom>
2023-09-18 13:29:39 -0600

    added/updated all docs

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -1,6 +1,7 @@
 library(tidyverse) #importing needed packages
 library(tidymodels)
 library(vroom)
+library(poissonreg)
 
 bike <- vroom("train.csv") #importing the data via vroom
 bike_test <-  vroom("test.csv") #importing test data
@@ -10,6 +11,7 @@ bike_test <-  vroom("test.csv") #importing test data
 bike <- bike %>% 
          mutate(weather = ifelse(weather >= 4, 3, weather)) %>%
          select(-casual, -registered)
+
 bike_test <- bike_test %>%
   mutate(weather = ifelse(weather >= 4, 3, weather))
 
@@ -19,8 +21,8 @@ my_recipe <- recipe(count ~ ., data=bike) %>%
   step_num2factor(season, levels=c("spring", "summer", "fall", "winter")) %>% #modifying season column from numbers to a factor
   step_num2factor(weather, levels=c("clear", "mist", "rain/snow")) %>% #modifying weather from numbers into factors
   step_bin2factor(holiday) %>% #modifying holiday to factor from numbers
-  step_bin2factor(workingday) %>%
-  step_rm(datetime)#modifying working day to factor from numbers 
+  step_bin2factor(workingday) %>% #modifying working day to factor from numbers 
+  step_rm(datetime)
 
 #updating table to reflect above changes, will be saved with original name "bike"
 prepped_recipe <- prep(my_recipe) 
@@ -49,3 +51,27 @@ bike_predictions$datetime <- bike_test$datetime
 bike_predictions$datetime <-format(bike_predictions$datetime)
 
 vroom_write(bike_predictions, "bike_predictions.csv", delim=",")
+
+
+
+#Poisson Model
+
+pois_mod <- poisson_reg() %>% #Type of model
+  set_engine("glm") # GLM = generalized linear model
+
+bike_pois_workflow <- workflow() %>%
+add_recipe(my_recipe) %>%
+add_model(pois_mod) %>%
+fit(data = bike) # Fit the workflow9
+
+bike_predictions_pois <- predict(bike_pois_workflow,
+                            new_data=bike_test) # Use fit to predict12
+
+
+bike_predictions_pois <- rename(bike_predictions_pois, count = .pred)
+
+
+bike_predictions_pois$datetime <- bike_test$datetime
+bike_predictions_pois$datetime <-format(bike_predictions_pois$datetime)
+
+vroom_write(bike_predictions_pois, "bike_predictions_pois.csv", delim=",")
